1c4553d4f9a474896e1354decb2c476f9ebd9b43
Meysa11 <sasytwins@gmail.com>
2026-09-24 14:02:08 +0700

    feat: perulangan array menu rekomendasi ke console (Aktivitas 6)

--- app.js
+++ app.js
@@ -207,17 +207,29 @@ console.log("Tier:", tierC);
 // TODO 6A:
 // Buat variabel Array bernama "menuRekomendasi" yang berisi minimal 5 nama menu kopi/makanan.
 
-
+let menuRekomendasi = [
+    "Cappuccino",
+    "kopi susu",
+    "Americano",
+    "salt bread",
+    "Cromboloni"
+];
+console.log(menuRekomendasi);
 
 
 // TODO 6B:
 // Gunakan perulangan "for loop" untuk mencetak setiap menu ke Console dengan format:
 // "1. Nama Menu", "2. Nama Menu", dst. Gunakan (i + 1) untuk nomor urutnya.
 
-
+console.log("Menu Rekomendasi");
+for (let i = 0; i < menuRekomendasi.length; i++) {
+    console.log((i + 1) + ". " + menuRekomendasi[i]);
+}
 
 
 // TODO 6C:
 // Cetak jumlah total menu di akhir daftar menggunakan properti ".length".
 // Akhiri program dengan: console.log("=== TUGAS MANDIRI SELESAI DENGAN SUKSES! ===");
 
+console.log("Total Menu:", menuRekomendasi.length);
+console.log("=== TUGAS MANDIRI SELESAI DENGAN SUKSES! ===");
